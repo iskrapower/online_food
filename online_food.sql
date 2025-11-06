bb896b65c1c061c5503f8302addc9c408dcf7db2
Student <student@dmuk.edu.kz>
2025-11-06 12:51:02 +0500

    Project

---
+++ online_food.sql
@@ -0,0 +1,27 @@
+create database online_food_database;
+use online_food_database;
+
+create table Item(
+	itemId int not null primary key auto_increment,
+    name varchar(150),
+    description varchar(4000),
+    unit_price int not null,
+    item_image blob
+);
+
+create table Customer(
+	customerId int not null primary key auto_increment,
+    name varchar(150),
+    profile blob
+);
+
+create table OrderItems(
+	orderId int not null primary key auto_increment,
+    itemId int not null,
+    customerId int not null,
+    delivery_status varchar(100),
+    quantity int not null,
+    foreign key (itemId) references Item(itemId),
+    foreign key (customerId) references Customer(customerId)
+);
+
